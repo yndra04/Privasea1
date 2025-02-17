fe92695313b071c489ff81663a880e7ebd424510
Dejavu <32336181+yndra04@users.noreply.github.com>
2025-02-17 11:51:00 +0700

    Update privasea.sh

--- privasea.sh
+++ privasea.sh
@@ -6,42 +6,8 @@ echo "/___/_//_/_/ |_/_/|_/___/ /___/ /_/   /_//_/\____/____/ "
 echo "               PRIVASEA NODE                            "
 sleep 3
 
-    echo "Mengatur Docker dalam rootless mode..."
-    sudo apt-get update
-    sudo apt install docker.io
-    sudo apt-get install -y docker-ce-rootless-extras
-    dockerd-rootless-setuptool.sh install
-
-    echo "Menambahkan rootless Docker ke PATH Anda."
-    echo "Tambahkan baris berikut ke file .bashrc Anda:"
-    echo "export PATH=\$HOME/bin:\$PATH"
-    echo "Lalu jalankan 'source ~/.bashrc'."
-
-    echo "export PATH=\$HOME/bin:\$PATH" >> ~/.bashrc
-    source ~/.bashrc
-
-    echo "Selesaikan setup dengan menjalankan: docker info"
-    echo "Rootless mode berhasil diatur!"
-
-echo "Memeriksa apakah container Docker dengan nama 'privanetix-node' ada..."
-
-if docker ps -a --format '{{.Names}}' | grep -q 'privanetix-node'; then
-    echo "Container 'privanetix-node' ditemukan. Menghentikan dan menghapus container..."
-    docker stop privanetix-node
-    docker rm privanetix-node
-    echo "Container 'privanetix-node' telah dihapus."
-else
-    echo "Container 'privanetix-node' tidak ditemukan."
-fi
-
-echo "Menarik image Docker 'privasea/acceleration-node-beta:latest'..."
-sudo docker pull privasea/acceleration-node-beta:latest
-
-echo "Menunggu 20 detik..."
-sleep 20
-
-echo "Membuat direktori ~/privasea/config..."
-sudo mkdir -p ~/privasea/config
+echo "Membuat direktori ~/privasea1/config..."
+sudo mkdir -p ~/privasea1/config
 
 echo "Menunggu 3 detik..."
 sleep 3
@@ -49,8 +15,8 @@ sleep 3
 echo "Masukkan isi untuk file 'wallet_keystore':"
 read -p "Masukkan data wallet: " wallet_data
 clear
-echo "$wallet_data" > ~/privasea/config/wallet_keystore
-echo "File 'wallet_keystore' berhasil dibuat di ~/privasea/config."
+echo "$wallet_data" > ~/privasea1/config/wallet_keystore
+echo "File 'wallet_keystore' berhasil dibuat di ~/privasea1/config."
 
 echo "Masukkan informasi keystore baru..."
 echo "Masukkan password untuk keystore: "
@@ -60,13 +26,13 @@ echo "Menunggu 3 detik..."
 sleep 3
 
 echo "Starting your Privasea Privanetix Node..."
-sudo docker run -d --name privanetix-node -v "$HOME/privasea/config:/app/config" -e KEYSTORE_PASSWORD=$KEYSTORE_PASSWORD privasea/acceleration-node-beta:latest
+sudo docker run -d --name privanetix-node1 -v "$HOME/privasea1/config:/app/config" -e KEYSTORE_PASSWORD=$KEYSTORE_PASSWORD privasea/acceleration-node-beta:latest
 sleep 5
 
-sudo docker restart privanetix-node
+sudo docker restart privanetix-node1
 sleep 5
 
-sudo docker logs privanetix-node
+sudo docker logs privanetix-node1
 sleep 1
 
 echo "Node setup is complete! Don't forget to Subscribe, like & share Youtube: SHARE IT HUB."
